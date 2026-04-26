5e9932f1727f7697e1eb025c946e7b85e893824e
robustam <robustam@gmail.com>
2026-04-26 12:42:13 -0400

    Interface

--- propiedad-service/src/main/java/com/inmobiliaria/propiedadservice/repository/PropiedadRepository.java
+++ propiedad-service/src/main/java/com/inmobiliaria/propiedadservice/repository/PropiedadRepository.java
@@ -4,4 +4,4 @@ import com.inmobiliaria.propiedadservice.model.Propiedad;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PropiedadRepository extends JpaRepository<Propiedad, Long> {
-}
+}//
