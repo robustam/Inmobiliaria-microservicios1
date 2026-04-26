8e8be73ca5717091d932fbef4468860979d598bd
robustam <robustam@gmail.com>
2026-04-26 12:37:35 -0400

    Controller

--- propiedad-service/src/main/java/com/inmobiliaria/propiedadservice/controller/PropiedadController.java
+++ propiedad-service/src/main/java/com/inmobiliaria/propiedadservice/controller/PropiedadController.java
@@ -1,4 +1,26 @@
 package com.inmobiliaria.propiedadservice.controller;
 
-public class PropiedadController { //Propiedad
+import com.inmobiliaria.propiedadservice.model.Propiedad;
+import com.inmobiliaria.propiedadservice.repository.PropiedadRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/propiedades")
+public class PropiedadController {
+
+    @Autowired
+    private PropiedadRepository repository;
+
+    @GetMapping
+    public List<Propiedad> listarTodas() {
+        return repository.findAll();
+    }
+
+    @PostMapping
+    public Propiedad guardar(@RequestBody Propiedad propiedad) {
+        return repository.save(propiedad);
+    }
 }
