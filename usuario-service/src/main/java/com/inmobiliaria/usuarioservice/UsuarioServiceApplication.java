9bbe9e694d7623005677af8ae80054c4942f7d21
robustam <robustam@gmail.com>
2026-04-26 13:38:57 -0400

    Service

--- usuario-service/src/main/java/com/inmobiliaria/usuarioservice/UsuarioServiceApplication.java
+++ usuario-service/src/main/java/com/inmobiliaria/usuarioservice/UsuarioServiceApplication.java
@@ -2,8 +2,11 @@ package com.inmobiliaria.usuarioservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UsuarioServiceApplication {
 
     public static void main(String[] args) {
