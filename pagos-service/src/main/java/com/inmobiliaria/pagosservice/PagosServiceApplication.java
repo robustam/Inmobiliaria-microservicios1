39e58259776b26167641c6739cb0fd3085c7f030
robustam <robustam@gmail.com>
2026-04-26 23:36:55 -0400

    Varios

---
+++ pagos-service/src/main/java/com/inmobiliaria/pagosservice/PagosServiceApplication.java
@@ -0,0 +1,13 @@
+package com.inmobiliaria.pagosservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagosServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PagosServiceApplication.class, args);
+    }
+
+}
