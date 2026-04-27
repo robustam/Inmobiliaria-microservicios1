1f2e45dc3e670b795cf2abd208bacf983830dc7c
robustam <robustam@gmail.com>
2026-04-26 21:26:51 -0400

    Controller

--- usuario-service/src/main/java/com/inmobiliaria/usuarioservice/controller/UsuarioController.java
+++ usuario-service/src/main/java/com/inmobiliaria/usuarioservice/controller/UsuarioController.java
@@ -1,5 +1,7 @@
 package com.inmobiliaria.usuarioservice.controller;
 
+import com.inmobiliaria.usuarioservice.client.PropiedadClient;
+import com.inmobiliaria.usuarioservice.dto.PropiedadDTO;
 import com.inmobiliaria.usuarioservice.model.Usuario;
 import com.inmobiliaria.usuarioservice.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,9 @@ public class UsuarioController {
     @Autowired
     private UsuarioRepository repository;
 
+    @Autowired
+    private PropiedadClient propiedadClient; // Inyectamos nuestro teléfono
+
     @GetMapping
     public List<Usuario> listarTodos() {
         return repository.findAll();
@@ -22,5 +27,12 @@ public class UsuarioController {
     @PostMapping
     public Usuario guardar(@RequestBody Usuario usuario) {
         return repository.save(usuario);
+    }
+
+    // NUEVO ENDPOINT: El usuario pide ver las propiedades
+    @GetMapping("/ver-propiedades")
+    public List<PropiedadDTO> verPropiedadesDesdeUsuario() {
+        // ¡Aquí ocurre la magia de la comunicación interna!
+        return propiedadClient.obtenerPropiedades();
     }
 }
