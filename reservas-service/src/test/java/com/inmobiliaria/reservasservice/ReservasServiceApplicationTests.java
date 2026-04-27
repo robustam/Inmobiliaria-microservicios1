64cf19c605aa4a78afc6280de63160ead31b80c5
robustam <robustam@gmail.com>
2026-04-26 21:29:15 -0400

    Otras configuraciones

---
+++ reservas-service/src/test/java/com/inmobiliaria/reservasservice/ReservasServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inmobiliaria.reservasservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
