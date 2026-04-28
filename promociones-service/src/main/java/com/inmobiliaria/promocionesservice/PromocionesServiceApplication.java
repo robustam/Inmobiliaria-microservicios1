b09bc3a31f366c101e919f18a66af13ab4cdf899
robustam <robustam@gmail.com>
2026-04-27 22:11:46 -0400

    Microservicio Promociones

---
+++ promociones-service/src/main/java/com/inmobiliaria/promocionesservice/PromocionesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.inmobiliaria.promocionesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromocionesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromocionesServiceApplication.class, args);
+	}
+
+}
---
+++ promociones-service/src/test/java/com/inmobiliaria/promocionesservice/PromocionesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inmobiliaria.promocionesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromocionesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
