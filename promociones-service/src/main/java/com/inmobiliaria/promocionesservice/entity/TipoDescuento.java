d8f5c41b550119b2b43106ff39d49a427ab8d134
robustam <robustam@gmail.com>
2026-04-27 22:11:25 -0400

    Microservicio Promociones

--- promociones-service/src/main/java/com/inmobiliaria/promocionesservice/entity/TipoDescuento.java
+++
@@ -1,6 +0,0 @@
-package com.inmobiliaria.promocionesservice.entity;
-
-public enum TipoDescuento {
-    PORCENTAJE,     // Descuento porcentual (15%)
-    MONTO_FIJO      // Descuento en monto fijo ($50)
-}
